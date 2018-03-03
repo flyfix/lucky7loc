f41a3f0075adf2d08fa715bc2ec8ee4754b01448
Simon Weis <laguna.ws@gmail.com>
2018-03-03 18:39:31 +0100

    server access cross origin

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ var path = require('path')
 var app = express()
 var routes = require('./routes')
 
-
-app.use('/', routes)
-
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', true);
@@ -16,6 +13,10 @@ app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
+
+app.use('/', routes)
+
+
 
 app.listen(8085, function () {
   console.log('RELEASE listening test on port 8085')
