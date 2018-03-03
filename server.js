f20eed068442ee49d66c6189832213d90968481f
Your Name <you@example.com>
2018-03-03 18:37:20 +0100

    asd

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ var routes = require('./routes')
 
 app.use('/', routes)
 
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.listen(8085, function () {
   console.log('RELEASE listening test on port 8085')
 })
